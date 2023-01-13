43146d446a2169b9d0d783a17500a65f78218cc9
m4medbekov <m4medbekov@gmail.com>
2023-01-14 00:02:40 +0300

    Update dotenv import + config

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import sequelize, { Sequelize } from "sequelize";
 import cookieParser, { CookieParseOptions } from "cookie-parser";
 
+dotenv.config();
+
 const PORT = process.env.PORT || 8080;
 
 const app: Express = express();
